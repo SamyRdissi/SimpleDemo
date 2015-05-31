f03b6c7c3a2fc3267b90b2a5523ce5a9458ea5db
SamyRdissi <samy94500@hotmail.com>
2015-05-31 20:30:14 +0100

    no message

---
+++ MyApplication/app/src/androidTest/java/com/example/samy/myapplication/MainActivityTest.java
@@ -0,0 +1,48 @@
+package com.example.samy.myapplication;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by samy R on 31/05/2015.
+ */
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testOnCreate() throws Exception {
+
+    }
+
+    @Test
+    public void testOnCreateOptionsMenu() throws Exception {
+
+    }
+
+    @Test
+    public void testOnOptionsItemSelected() throws Exception {
+
+    }
+
+    @Test
+    public void testOnItemSelected() throws Exception {
+
+    }
+
+    @Test
+    public void testOnNothingSelected() throws Exception {
+
+    }
+}
