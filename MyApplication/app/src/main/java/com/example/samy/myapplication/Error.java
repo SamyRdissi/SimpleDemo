1c52c85ec1d2de1fe9f6b66e5257ca5c53890db2
Samy Rdissi <samyrdissi@Samys-iMac.local>
2017-01-30 17:20:21 +0100

    add names

--- MyApplication/app/src/main/java/com/example/samy/myapplication/Error.java
+++ MyApplication/app/src/main/java/com/example/samy/myapplication/Error.java
@@ -2,4 +2,8 @@ package com.example.samy.myapplication;
 
 
 public class Error {
+    private static final String name1 = "name1";
+    private static final String name2 = "name2";
+    private static final String name3 = "name3";
+    
 }
